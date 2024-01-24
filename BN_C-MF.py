64a6f84ccd2c0d965d4b5d9ce1f48301345137cc
Yanfei Jiao <114814007+Tiffany-XQ@users.noreply.github.com>
2024-01-24 12:24:03 +0800

    Add files via upload

---
+++ BN_C-MF.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import time
+import os
+import logging
+import traceback
+import ccxt
+
+BINANCE_CONFIG = {
+    'apiKey': '',
+    'secret': '',
+    'timeout': 8000,
+    'rateLimit': 10,
+    'verbose': False,
+    'hostname': 'binancezh.com',
+    'enableRateLimit': False}
+
+EXCHANGE = ccxt.binance(BINANCE_CONFIG)
+
+MAX_TRIAL = 3
+
+ROOT_PATH = os.getcwd()
+CMF_PATH = os.path.join(ROOT_PATH, 'Database', EXCHANGE.id, 'C-MF')
+
+def save_klines(params, date, symbol, time_interval):
+    
+    df = EXCHANGE.dapiPublicGetKlines(params=params)    # Coin-M feature: dapi
+    df = pd.DataFrame(df, dtype=float,
+                    columns=['Open_time', 'Open', 'High', 'Low', 'Close', 'Volume',
+                            'Close_time', 'Base_asset_vol', 'Num_trades',
+                            'Taker_buy_vol', 'Taker_buy_base_asset_vol', 
+                            'Ignore'])
+    df['Open_time'] = pd.to_datetime(df['Open_time'], unit='ms')
+    df['Close_time'] = pd.to_datetime(df['Close_time'], unit="ms")
+    df.drop(["Ignore"], axis=1)
+
+    path = os.path.join(CMF_PATH, str(pd.Timestamp(date).date()))
+    os.makedirs(path, exist_ok=True)
+    file_name = '_'.join([symbol, time_interval]) + '.csv'
+    path = os.path.join(path, file_name)
+
+    df.to_csv(path, index=False)
+
+
+def main(date):
+
+    Symbols = ['BTCUSD_PERP', 'BTCUSD_221230', 'BTCUSD_230331', 'ETHUSD_PERP', 'ETHUSD_221230', 'ETHUSD_230331']
+
+    Intervals = ['5m', '15m', '30m']
+
+    start_time = EXCHANGE.parse8601(date)
+    end_time = pd.Timestamp(date)+pd.Timedelta("1 day")
+    end_time = EXCHANGE.parse8601(str(end_time))
+
+    logging.basicConfig(format='%(asctime)s || %(message)s', datefmt='%Y-%m-%d %H:%M:%S', 
+                        level=logging.INFO)
+
+    for symbol in Symbols:
+
+        for time_interval in Intervals:
+
+            logging.info(f'Fetching Binance {pd.Timestamp(date).date()} {symbol} {time_interval} COIN-M Features klines...')
+
+            for _ in range(MAX_TRIAL):
+
+                try:
+                    params = {'symbol': symbol, 'interval': time_interval,
+                            'limit': 1000, 'startTime': start_time, 'endTime': end_time}
+                    save_klines(params, date, symbol, time_interval)
+                    logging.info('Fetching success.')
+                    time.sleep(2)
+                    break
+
+                except:
+                    traceback.print_exc()
+                    logging.info(f'Fetching fail.')
+                    continue
+
+
+if __name__ == '__main__':
+    Start = "2022-01-01"
+    End = "2022-01-03"
+
+    Date = pd.date_range(start=Start, end=End, freq='1D')
+    Date = Date.strftime("%Y-%m-%d 00:00:00")
+
+    for date in Date:
+        main(date)
